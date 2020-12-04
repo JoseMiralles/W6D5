ea28e1af829bb51e97b5ad7c0ae70b8461012d86
Jose Miralles <josemiralles1991@gmail.com>
2020-12-04 15:45:57 -0500

    Finished index view and controller!

--- cats_app/app/controllers/cats_controller.rb
+++ cats_app/app/controllers/cats_controller.rb
@@ -1,9 +1,11 @@
 class CatsController < ApplicationController
 
     def index
+        # @cats = Cat.all.includes(:name, :birth_date, :color, :sex, :description)
         @cats = Cat.all
         render :index
     end
+    
     def show
         @cat = Cat.find_by(id: params[:id])
         if @cat 
